6cd06bc88f500993ee84202a844c5ec450cee763
enrisco <enriquerodrigues790@gmail.com>
2022-08-31 12:52:19 -0300

    v0.7.0
    - bugs fixes

--- projAbmooction/Assets/Scripts/Controllers/DialogBoxBuilderController.cs
+++ projAbmooction/Assets/Scripts/Controllers/DialogBoxBuilderController.cs
@@ -12,34 +12,48 @@ public class DialogBoxBuilderController : MonoBehaviour
 
     public IEnumerator ShowTyped(string label, string content, bool type)
     {
-        GameObject d = Instantiate(DialogBox, transform);
-        DialogBoxController c = d.GetComponent<DialogBoxController>();
-        c.SetType(label, content, type);
+        if (!CheckIfExistAnotherDialogInstance())
+        {
+            GameObject d = Instantiate(DialogBox, transform);
+            DialogBoxController c = d.GetComponent<DialogBoxController>();
+            c.SetType(label, content, type);
 
-        yield return new WaitUntil(() => c.button != ButtonPressed.Null);
-        LastButtonState = c.button;
-        Destroy(d);
+            yield return new WaitUntil(() => c.button != ButtonPressed.Null);
+            LastButtonState = c.button;
+            Destroy(d);
+        }
     }
 
     public IEnumerator ShowImage(string label, string content, string yes, string no, Sprite image, Vector3 scaleImage)
     {
-        GameObject d = Instantiate(DialogBoxImage, transform);
-        DialogBoxImageController c = d.GetComponent<DialogBoxImageController>();
-        c.SetDialogBox(label, content, image, yes, no, scaleImage);
+        if (!CheckIfExistAnotherDialogInstance())
+        {
+            GameObject d = Instantiate(DialogBoxImage, transform);
+            DialogBoxImageController c = d.GetComponent<DialogBoxImageController>();
+            c.SetDialogBox(label, content, image, yes, no, scaleImage);
 
-        yield return new WaitUntil(() => c.button != ButtonPressed.Null);
-        LastButtonState = c.button;
-        Destroy(d);
+            yield return new WaitUntil(() => c.button != ButtonPressed.Null);
+            LastButtonState = c.button;
+            Destroy(d);
+        }
     }
 
     public IEnumerator ShowSlider(string label, string content, bool yesNo, Sprite image, float percent)
     {
-        GameObject d = Instantiate(DialogBoxSlider, transform);
-        DialogBoxSliderController c = d.GetComponent<DialogBoxSliderController>();
-        c.SetDialogBox(label, content, image, yesNo, percent);
+        if (!CheckIfExistAnotherDialogInstance())
+        {
+            GameObject d = Instantiate(DialogBoxSlider, transform);
+            DialogBoxSliderController c = d.GetComponent<DialogBoxSliderController>();
+            c.SetDialogBox(label, content, image, yesNo, percent);
 
-        yield return new WaitUntil(() => c.button != ButtonPressed.Null);
-        LastButtonState = c.button;
-        Destroy(d);
+            yield return new WaitUntil(() => c.button != ButtonPressed.Null);
+            LastButtonState = c.button;
+            Destroy(d);
+        }
+    }
+
+    bool CheckIfExistAnotherDialogInstance()
+    {
+        return transform.childCount > 0;
     }
 }
